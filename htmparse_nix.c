61e02089c9a81ed71a2712b9d94ceee213ae4e80
Debabrata Choudhury <drake@localhost.localdomain>
2016-11-18 11:52:21 +0530

    parsing done!

--- htmparse_nix.c
+++ htmparse_nix.c
@@ -7,6 +7,8 @@ struct data
 	char ID[10];
 	char name[30];
 	int date,mth,yr;
+	int inpTime;
+	int outTime;
 
 };
 
@@ -17,8 +19,7 @@ typedef struct data dt;
 
 
 int readDate(char ch[]);
-void readNxtLine(long n);
-void readTime(long n );
+long readNxtLine(long n);
 
 
 int main()
@@ -45,14 +46,22 @@ int main()
 			break;
 		j=readDate(lines);
 		ch=getc(fp);
-		//if(j==1)
-		//{
-	//		readNxtLine(ftell(fp));
-	//		break;
-	//	}
+	if(j==1)
+	{
+		k1=readNxtLine(ftell(fp));
+		k1=readNxtLine(k1);
+		k1=readNxtLine(k1);
+		k1=readNxtLine(k1);
+		k1=readNxtLine(k1);
+
+	}
 
 	}
 	fclose(fp);
+	for(i=0;i<150;i++)
+	{
+		puts(implines[i]);
+	}
 	return 0;
 }
 
@@ -61,17 +70,17 @@ int readDate(char lines[])
 	int i=0;
 	while(lines[i]!='\0')
 	{
-		if(lines[i]=='2' && lines[++i]=='-')
+		if((lines[i]=='0' || lines[i]=='1' || lines[i]=='2' || lines[i]=='3' || lines[i]=='4' || lines[i]=='5' || lines[i]=='6' || lines[i]=='7' || lines[i]=='8' || lines[i]=='9' ) && lines[++i]=='-' && (lines[i+2]=='0' || lines[i+2]=='1' || lines[i+2]=='2' || lines[i+2]=='3' || lines[i+2]=='4' || lines[i+2]=='5' || lines[i+2]=='6' || lines[i+2]=='7' || lines[i+2]=='8' || lines[i+2]=='9' ) )
 		{
-			fprintf(stdout,"%s\n",lines);
-			//strcpy(implines[cmt++],lines);
+			//fprintf(stdout,"%s\n",lines);
+			strcpy(implines[cmt++],lines);
 			return 1;		
 		}
 		i++;
 	}
 }
 
-/*void readNxtLine(long n)
+long readNxtLine(long n)
 {
 	FILE *fp;
 	fp=fopen("Programme.html","r");
@@ -87,7 +96,10 @@ int readDate(char lines[])
        	    break;
 	}
 	lines[i]='\0';
-	//strcpy(implines[cmt++],lines);
-	printf("%s\n",lines);
+	strcpy(implines[cmt++],lines);
+	//printf("%s\n",lines);
+	ch=getc(fp);
+	return ftell(fp);
 	fclose(fp);
-}*/
+
+}
