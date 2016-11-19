e28478eccfccf3480aec40943013c43638cfbcc1
Debabrata Choudhury <drake@localhost.localdomain>
2016-11-19 22:05:54 +0530

    Parsing Completed

--- htmparse_nix.c
+++ htmparse_nix.c
@@ -4,31 +4,37 @@
 struct data
 {
 	
-	char ID[10];
-	char name[30];
-	int date,mth,yr;
-	int inpTime;
-	int outTime;
+	char stat[10];// TO STORE THE STATUS OF THE DAY 
+	char name[30];// TO STORE THE NAME AND ID
+	char date[50];// TO STORE THE DATE
+	char inpTime[20];// TO STORE THE INPUT TIME
+	char toTime[20];// TO STORE THE TOTAL TIME
+	char outTime[20];// TO STORE THE OUTPUT TIME
 
 };
 
 int cmt=0;
 char implines[200][100];
 typedef struct data dt;
+dt ht;
 
-
-
-int readDate(char ch[]);
-long readNxtLine(long n);
-
+int readDate(char ch[]); // TO READ THE FIRST DATA FROM HTML
+long readNxtLine(long n);// TO READ NEXT LINES FROM FIRST DATA FROM HTML
+int lenMLines();// TO PRODCUCE THE NUMBER OF DATA LINES
+void getDate(char ch[]);// TO EXTRACT DATE 
+void getIDName(); // TO EXTRACT ID AND NAME
+void getinTime(); // TO EXTRACT IN TIME
+void getoutTime(); // TO EXTRACT OUT TIME
+void gettotalTime(); // TO EXTRACT TOTAL TIME
+void getstat();// TO EXTRACT THE STATUS OF THE DAY
 
 int main()
 {
-	FILE *fp;
-
+	FILE *fp,*fp1;
+	fp1=fopen("db.dat","w");
 	fp=fopen("Programme.html","r");
 	char lines[5000],ch;
-	int i=0,j;
+	int i=0,j,len;
 	long k1;
 	ch=getc(fp);
 	while(ch!=EOF)
@@ -58,10 +64,25 @@ int main()
 
 	}
 	fclose(fp);
-	for(i=0;i<150;i++)
+	len=lenMLines();
+	i=-1;
+	while(i<125)
 	{
-		puts(implines[i]);
+		getDate(implines[++i]);
+		fprintf(fp1,"%s\t",ht.date);
+		getIDName(implines[++i]);
+		fprintf(fp1,"%s\t",ht.name);
+		getinTime(implines[++i]);
+		fprintf(fp1,"%s\t",ht.inpTime);
+		getoutTime(implines[++i]);
+		fprintf(fp1,"%s\t",ht.outTime);
+		gettotalTime(implines[++i]);
+		fprintf(fp1,"%s\t",ht.toTime);
+		getstat(implines[++i]);
+		fprintf(fp1,"%s\n",ht.stat);
+		//i++;
 	}
+		fclose(fp1);
 	return 0;
 }
 
@@ -72,7 +93,6 @@ int readDate(char lines[])
 	{
 		if((lines[i]=='0' || lines[i]=='1' || lines[i]=='2' || lines[i]=='3' || lines[i]=='4' || lines[i]=='5' || lines[i]=='6' || lines[i]=='7' || lines[i]=='8' || lines[i]=='9' ) && lines[++i]=='-' && (lines[i+2]=='0' || lines[i+2]=='1' || lines[i+2]=='2' || lines[i+2]=='3' || lines[i+2]=='4' || lines[i+2]=='5' || lines[i+2]=='6' || lines[i+2]=='7' || lines[i+2]=='8' || lines[i+2]=='9' ) )
 		{
-			//fprintf(stdout,"%s\n",lines);
 			strcpy(implines[cmt++],lines);
 			return 1;		
 		}
@@ -97,9 +117,209 @@ long readNxtLine(long n)
 	}
 	lines[i]='\0';
 	strcpy(implines[cmt++],lines);
-	//printf("%s\n",lines);
 	ch=getc(fp);
 	return ftell(fp);
 	fclose(fp);
 
 }
+int lenMLines()
+{
+	int i=0,c=0;
+	while(implines[i][0])
+	{
+		c++;
+		i++;
+	}
+	return c;
+}
+
+
+void getDate(char ch[])
+{
+	int k=0,j=0,i=0,h=0;
+	int len=strlen(ch);
+	for(i=1;i<len;i++)
+	{
+		if(ch[i]=='<')
+		{
+			while(1)
+			{
+				if(ch[i]=='>')
+				{
+					break;
+				}
+				i++;
+			}
+			i++;
+			while(1)
+			{
+				if(ch[i]=='<')
+					break;
+				ht.date[k]=ch[i];
+				i++;
+				k++;
+			}
+			break;
+		}
+
+	}
+	
+}
+
+void getIDName(char ch[])
+{
+	int k=0,j=0,i=0,h=0;
+	int len=strlen(ch);
+	for(i=1;i<len;i++)
+	{
+		if(ch[i]=='<')
+		{
+			while(1)
+			{
+				if(ch[i]=='>')
+				{
+					break;
+				}
+				i++;
+			}
+			i++;
+			while(1)
+			{
+				if(ch[i]=='<')
+					break;
+				ht.name[k]=ch[i];
+				i++;
+				k++;
+			}
+			break;
+		}
+
+	}
+	
+}
+
+void getinTime(char ch[])
+{
+	int k=0,j=0,i=0,h=0;
+	int len=strlen(ch);
+	for(i=1;i<len;i++)
+	{
+		if(ch[i]=='<')
+		{
+			while(1)
+			{
+				if(ch[i]=='>')
+				{
+					break;
+				}
+				i++;
+			}
+			i++;
+			while(1)
+			{
+				if(ch[i]=='<')
+					break;
+				ht.inpTime[k]=ch[i];
+				i++;
+				k++;
+			}
+			break;
+		}
+
+	}
+}
+
+void getoutTime(char ch[])
+{
+	int k=0,j=0,i=0,h=0;
+	int len=strlen(ch);
+	for(i=1;i<len;i++)
+	{
+		if(ch[i]=='<')
+		{
+			while(1)
+			{
+				if(ch[i]=='>')
+				{
+					break;
+				}
+				i++;
+			}
+			i++;
+			while(1)
+			{
+				if(ch[i]=='<')
+					break;
+				ht.outTime[k]=ch[i];
+				i++;
+				k++;
+			}
+			break;
+		}
+
+	}
+}
+
+
+void gettotalTime(char ch[])
+{
+	int k=0,j=0,i=0,h=0;
+	int len=strlen(ch);
+	for(i=1;i<len;i++)
+	{
+		if(ch[i]=='<')
+		{
+			while(1)
+			{
+				if(ch[i]=='>')
+				{
+					break;
+				}
+				i++;
+			}
+			i++;
+			while(1)
+			{
+				if(ch[i]=='<')
+					break;
+				ht.toTime[k]=ch[i];
+				i++;
+				k++;
+			}
+			break;
+		}
+
+	}
+}
+
+void getstat(char ch[])
+{
+	int k=0,j=0,i=0,h=0;
+	int len=strlen(ch);
+	for(i=1;i<len;i++)
+	{
+		if(ch[i]=='<')
+		{
+			while(1)
+			{
+				if(ch[i]=='>')
+				{
+					break;
+				}
+				i++;
+			}
+			i++;
+			while(1)
+			{
+				if(ch[i]=='<')
+					break;
+				ht.stat[k]=ch[i];
+				i++;
+				k++;
+			}
+			break;
+		}
+
+	}
+}
+
